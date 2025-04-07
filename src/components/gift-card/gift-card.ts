4ca7caf69557f23c840d306f80422d3b99e17143
Regina <rmyachina@mail.ru>
2025-04-07 18:28:08 +0300

    feat:gift-card

---
+++ src/components/gift-card/gift-card.ts
@@ -0,0 +1,70 @@
+function convertToPercents(hundredPerInPx: number, valueInPx: number) {
+    return Math.round((valueInPx * 100) / hundredPerInPx)
+}
+
+void (function () {
+    const scrollContainer = document.querySelector<HTMLElement>('.gift-card__scroll')
+    const container = document.querySelector<HTMLElement>('.gift-card .container')
+    const initialScrollContent = document.querySelector<HTMLElement>('.gift-card__scroll-content')
+    if (!container || !scrollContainer || !initialScrollContent) return
+
+    const containerWidth = container.offsetWidth
+    const containerEnd = container.getBoundingClientRect().right
+    const scrollContentWidth = initialScrollContent.clientWidth
+    const margin = containerWidth - scrollContentWidth
+
+    const addTitle = (translate: number) => {
+        console.log(translate)
+        const scrollContent = document.createElement('div')
+        scrollContent.classList.add('gift-card__scroll-content')
+
+        const title = document.createElement('div')
+        title.textContent = 'Подарочная карта'
+        title.classList.add('gift-card__title')
+
+        const icon = document.createElement('div')
+        icon.classList.add('icon', 'icon--arrow-right')
+
+        scrollContent.append(title, icon)
+        scrollContainer.append(scrollContent)
+        const titleMarginRight = parseInt(window.getComputedStyle(title).marginRight)
+        const _translate = translate > scrollContentWidth ? scrollContentWidth * -1 : translate + titleMarginRight
+        scrollContent.style.translate = `${_translate}px`
+    }
+
+    const checkWidth = () => {
+        const titles = document.querySelectorAll<HTMLElement>('.gift-card__scroll-content')
+        let titlesWidth = 0
+        titles.forEach((title) => (titlesWidth += title.offsetWidth))
+        const space = containerWidth - titlesWidth
+
+        if (space > 0 || space > scrollContentWidth * -1) {
+            addTitle(titlesWidth)
+            checkWidth()
+        } else {
+            titles.forEach((title) => {
+                let translate = parseInt(title.style.translate) || 0
+                const titleMarginRight = parseInt(window.getComputedStyle(title).marginRight)
+
+                let timeOut = setTimeout(function move() {
+                    const titleStart = title.getBoundingClientRect().left
+                    const width = title.offsetWidth
+                    if (titleStart > containerEnd) {
+                        scrollContainer.append(title)
+                        translate = -width - margin
+                        title.style.translate = `${translate}px`
+                    } else {
+                        translate += 200
+                    }
+
+                    title.style.translate = `${translate}px`
+                    timeOut = setTimeout(move, 1000)
+                }, 1000)
+            })
+        }
+    }
+
+    checkWidth()
+})()
+
+function initTitleScroll() {}
---
+++ src/pages/index/main-page.ts
@@ -0,0 +1,1 @@
+import 'components/gift-card/gift-card'
