9da7039782d983a389e453118867aec0a14ac859
Regina <rmyachina@mail.ru>
2025-04-09 09:27:01 +0300

    feat: size-slider

--- src/components/size/size.ts
+++ src/components/size/size.ts
@@ -1,4 +1,5 @@
 import Swiper from 'swiper'
+import { Navigation } from 'swiper/modules'
 
 void (function () {
     const sizeSlider = document.querySelector<HTMLElement>('.size__list .swiper')
@@ -6,7 +7,19 @@ void (function () {
     if (!sizeSlider) return
 
     new Swiper(sizeSlider, {
-        slidesPerView: 5,
-        spaceBetween: 14,
+        modules: [Navigation],
+        slidesPerView: 4.5,
+        spaceBetween: 10,
+        navigation: {
+            nextEl: '.size__list-navigation-next',
+            prevEl: '.size__list-navigation-prev',
+        },
+
+        breakpoints: {
+            1000: {
+                slidesPerView: 5,
+                spaceBetween: 14,
+            },
+        },
     })
 })()
