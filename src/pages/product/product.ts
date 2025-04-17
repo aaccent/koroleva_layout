877f2a64e1ccb9d0cc33f6c6f9aed430a9d21baf
Sevastyan <seva@aaccent.ru>
2025-04-17 18:41:28 +0300

    feat: images popup

--- src/pages/product/product.ts
+++ src/pages/product/product.ts
@@ -1,1 +1,10 @@
 import 'components/product-hero/product-hero'
+import { isDesktop } from 'globals/adaptive'
+import { openPopup } from 'features/popup/popup'
+
+document.querySelector('.product-hero__images')?.addEventListener('click', (event) => {
+    if (isDesktop) return
+    if (!(event.target as HTMLElement).classList.contains('product-hero__images-item')) return
+
+    openPopup('images')
+})
