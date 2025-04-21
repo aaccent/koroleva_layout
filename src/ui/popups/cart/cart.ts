45ff9508f4c0fce3afa21f4047808c55a5394e36
Sevastyan <seva@aaccent.ru>
2025-04-21 10:33:24 +0300

    refactor: remove cart scripts

--- src/ui/popups/cart/cart.ts
+++ src/ui/popups/cart/cart.ts
@@ -1,5 +1,5 @@
-import { testCartInfo, testCartProducts } from '@/test-cart'
 import { isMobile } from 'globals/adaptive'
+import { createMutationObserver } from 'features/mutationObserver'
 
 export type ProductInfo =
     | {
@@ -29,16 +29,25 @@ export interface CartInfo {
     discount?: number
 }
 
-interface DataItemElement extends HTMLElement {
-    dataset: {
-        item: keyof ProductInfo
-    }
-}
+function stickyCardInfo() {
+    const PADDING = 48
+    const cartList = document.querySelector<HTMLElement>('.cart__list')
+    const cartInfo = document.querySelector<HTMLElement>('.cart__info')
+    if (!cartInfo || !cartList) return
 
-interface CartInfoElement extends HTMLElement {
-    dataset: {
-        info: keyof CartInfo
+    if (cartList.childElementCount <= 2) {
+        cartList.removeAttribute('style')
+        cartInfo.classList.remove('_sticky')
+        return
     }
+
+    cartInfo.classList.add('_sticky')
+    const cartHeader = document.querySelector<HTMLElement>('.cart__header')
+    const cartInfoTop = cartInfo.getBoundingClientRect().top
+    const cartHeaderBottom = cartHeader?.getBoundingClientRect().bottom || 0
+    const cartListHeight = cartInfoTop - cartHeaderBottom - PADDING
+
+    cartList.style.height = cartListHeight + 'px'
 }
 
 function init() {
@@ -48,130 +57,10 @@ function init() {
     const cartButton = document.querySelector('.cart__button') as HTMLElement
     cartButton.textContent = isMobile ? 'к оформлению' : 'Перейти к оформлению'
 
-    function stickyCardInfo() {
-        const PADDING = 48
-        const cartList = document.querySelector<HTMLElement>('.cart__list')
-        const cartInfo = document.querySelector<HTMLElement>('.cart__info')
-        if (!cartInfo || !cartList) return
+    const cartList = document.querySelector('.cart__list')
 
-        if (cartList.childElementCount <= 2) {
-            cartList.removeAttribute('style')
-            cartInfo.classList.remove('_sticky')
-            return
-        }
+    if (!cartList) return
 
-        cartInfo.classList.add('_sticky')
-        const cartHeader = document.querySelector<HTMLElement>('.cart__header')
-        const cartInfoTop = cartInfo.getBoundingClientRect().top
-        const cartHeaderBottom = cartHeader?.getBoundingClientRect().bottom || 0
-        const cartListHeight = cartInfoTop - cartHeaderBottom - PADDING
-
-        cartList.style.height = cartListHeight + 'px'
-    }
-
-    function updateItemInfo(item: HTMLElement, info: ProductInfo) {
-        switch (info.type) {
-            case 'product':
-                Object.entries(info).forEach(([key, value]) => {
-                    const dataEl = item.querySelector<DataItemElement>(`[data-item = ${key}]`)
-                    switch (key) {
-                        case 'amount':
-                            if (!value) {
-                                item.classList.add('_out-of-stock')
-                            } else {
-                                item.classList.remove('_out-of-stock')
-                            }
-                    }
-                    if (!dataEl) return
-                    if (dataEl instanceof HTMLImageElement) {
-                        dataEl.src = value.toString()
-                        return
-                    }
-
-                    dataEl.textContent = key === 'price' || key === 'discountPrice' ? value + '₽' : value.toString()
-                })
-                break
-            case 'gift-card':
-                const image = item.querySelector<HTMLImageElement>('.product-item__image')
-                const title = item.querySelector('.product-item__title')
-                const size = item.querySelector('.product-item__size')
-                const amount = item.querySelector('.product-item__amount-value')
-                const price = item.querySelector('.product-item__price')
-
-                if (!image || !title || !size || !amount || !price) return
-
-                image.src = 'assets/static/gift-card-in-cart.png'
-                title.textContent = 'подарочная карта'
-                size.textContent = `Номинал: ${info.price} ₽`
-                amount.textContent = info.amount.toString()
-                price.textContent = info.price + '₽'
-                break
-        }
-    }
-
-    const addItems = (list: ProductInfo[]) => {
-        document.querySelector('.cart__inner')?.classList.remove('_empty')
-        console.log('pop')
-
-        const cartList = document.querySelector<HTMLElement>('.cart__list')
-        const layout = cartPopup.querySelector<HTMLElement>('.product-item._layout')
-        if (!cartList || !layout) return
-
-        list.forEach((info) => {
-            const itemInCart = cartList.querySelector<HTMLElement>(`.product-item[data-id='${info.id}']`)
-            if (itemInCart) {
-                updateItemInfo(itemInCart, info)
-                return
-            }
-
-            const clone = layout.cloneNode(true) as HTMLElement
-            clone.classList.remove('_layout')
-            clone.dataset.id = info.id.toString()
-
-            updateItemInfo(clone, info)
-            cartList.append(clone)
-        })
-
-        stickyCardInfo()
-    }
-
-    const clearCart = () => {
-        const items = cartPopup.querySelectorAll('.product-item')
-        const cartInner = document.querySelector('.cart__inner')
-        items.forEach((items) => items.remove())
-        cartInner?.classList.add('_empty')
-
-        const cartAmount = document.querySelector('.cart__title span')
-        if (cartAmount) cartAmount.textContent = '0'
-    }
-
-    const removeItem = (id: number | string) => {
-        const itemToRemove = cartPopup.querySelector(`.product-item[data-id='${id}']`)
-        const lastItem = !(itemToRemove?.nextElementSibling || itemToRemove?.previousElementSibling)
-
-        itemToRemove?.remove()
-        stickyCardInfo()
-        if (lastItem) clearCart()
-    }
-
-    function setCartInfo(info: CartInfo) {
-        const cartInfoElements = document.querySelectorAll<CartInfoElement>('.cart__info-item[data-info]')
-        cartInfoElements.forEach((element) => {
-            const dataInfo = element.dataset.info
-            const value = element.firstElementChild
-            if (!value) return
-
-            if (!!info[dataInfo]) {
-                element.classList.remove('_empty')
-                value.textContent =
-                    dataInfo === 'price' || dataInfo === 'discount' ? info[dataInfo] + ' ₽' : info[dataInfo]?.toString()
-            } else {
-                element.classList.add('_empty')
-            }
-        })
-    }
-
-    addItems(testCartProducts)
-    setCartInfo(testCartInfo)
+    createMutationObserver(cartList, stickyCardInfo)
 }
 init()
