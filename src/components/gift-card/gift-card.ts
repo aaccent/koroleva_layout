39deba63f0db2dc87813d4493dab372d1b751faf
Regina <rmyachina@mail.ru>
2025-05-05 16:41:34 +0300

    feat: running line

--- src/components/gift-card/gift-card.ts
+++ src/components/gift-card/gift-card.ts
@@ -36,44 +36,29 @@ function addScrollContent(number: number = 1) {
 void (function () {
     addScrollContent()
 
-    // const titles = document.querySelectorAll<HTMLElement>('.gift-card__scroll-content')
-    // const scrollContainer = document.querySelector<HTMLElement>('.gift-card__scroll')
-    // if (!scrollContainer) return
-    //
-    // console.log(scrollContainer.offsetWidth)
-    // console.log(titles[0].offsetWidth)
-    //
-    // //Сколько % занимает длинна строки от длинны контейнера
-    // const titlePercent = Math.round((titles[0].offsetWidth * 100) / scrollContainer.offsetWidth)
-    // console.log('titlePercent ' + titlePercent)
-    //
-    // //Сколько % ширина контенера относительно ширины строки
-    // const containerPercent = Math.round((scrollContainer.offsetWidth * 100) / titles[0].offsetWidth)
-    //
-    // console.log('containerPercent ' + containerPercent)
-    //
-    // //На сколько % сумма ширин строк больше ширины контейнера
-    // const extraTranslate =
-    //     containerPercent < 100
-    //         ? 100 - containerPercent
-    //         : ((titles[0].offsetWidth * (titles.length - 1) - scrollContainer.offsetWidth) * 100) /
-    //           scrollContainer.offsetWidth
-    // console.log('extraTranslate ' + extraTranslate)
-    //
-    // let timeout = setTimeout(function move() {
-    //     titles.forEach((title) => {
-    //         const translate = parseInt(title.style.translate) || 0
-    //         if (translate > containerPercent) {
-    //             console.log(containerPercent, translate)
-    //             scrollContainer.append(title)
-    //             title.style.translate = `-${100 + extraTranslate}%`
-    //         } else {
-    //             title.style.translate = `${translate + 1}%`
-    //         }
-    //     })
-    //
-    //     timeout = setTimeout(move, 100)
-    // }, 100)
+    const titles = document.querySelectorAll<HTMLElement>('.gift-card__scroll-content')
+    const scrollContainer = document.querySelector<HTMLElement>('.gift-card__scroll')
+    if (!scrollContainer) return
+
+    const maxTranslate = Math.round((scrollContainer.offsetWidth * 100) / titles[0].offsetWidth)
+
+    const extraTranslatePx = titles[0].offsetWidth * (titles.length - 1) - scrollContainer.offsetWidth
+    const extraTranslatePercent = Math.round((100 * extraTranslatePx) / titles[0].offsetWidth)
+
+    let timeout = setTimeout(function move() {
+        titles.forEach((title) => {
+            const translate = parseInt(title.style.translate) || 0
+
+            if (translate >= maxTranslate) {
+                title.style.translate = `-${100 + extraTranslatePercent - 10}%`
+                scrollContainer.append(title)
+            } else {
+                title.style.translate = `${translate + 10}%`
+            }
+        })
+
+        timeout = setTimeout(move, 1000)
+    }, 1000)
 
     /** Выбор номинала карты */
     const cardValues = document.querySelectorAll('.gift-card__list-item')
