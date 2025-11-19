cf139a37147fa86d13740938e6e135220c11a1b5
Sevastyan <seva@aaccent.ru>
2025-11-19 09:26:41 +0300

    feat: fix main hero slide autoplay

--- src/components/main-hero/main-hero.ts
+++ src/components/main-hero/main-hero.ts
@@ -1,6 +1,14 @@
 import Swiper from 'swiper'
 import { Autoplay, EffectFade, Navigation, Pagination } from 'swiper/modules'
 
+interface SwiperSlideElement extends HTMLElement {
+    dataset: {
+        swiperSlideIndex: string
+    }
+}
+
+const DEFAULT_DELAY = '5'
+
 function createSVGCircle(container: Element) {
     const innerSvg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
     innerSvg.setAttribute('viewBox', '0 0 85 85')
@@ -30,19 +38,13 @@ void (function () {
     const mainHeroSlider = document.querySelector<HTMLElement>('.main-hero__slider')
     if (!mainHeroSlider) return
 
-    const firstSlideDelay = document.querySelectorAll<HTMLElement>('.main-hero__slide')[0].dataset.delay || '3'
+    const circlesPreview = document.querySelectorAll<HTMLElement>('.main-hero__thumbs-slide')
 
     const mainSwiper = new Swiper(mainHeroSlider, {
         modules: [Autoplay, EffectFade, Navigation, Pagination],
         effect: 'fade',
         slidesPerView: 1,
         loop: true,
-        fadeEffect: {
-            crossFade: true,
-        },
-        autoplay: {
-            delay: parseInt(firstSlideDelay) * 1000,
-        },
 
         allowTouchMove: false,
         pagination: {
@@ -53,44 +55,53 @@ void (function () {
             prevEl: '.main-hero__slider-navigation-prev',
             nextEl: '.main-hero__slider-navigation-next',
         },
+        on: {
+            init: (swiper) => {
+                const circlesCount = circlesPreview.length
+                const translate = circlesCount <= 5 ? 50 : 70
+
+                /** Позиционирует круглые превью;
+                 *  Создает SVG вокруг них;
+                 *  Записывает количество секунд анимации круга в css переменную
+                 */
+                circlesPreview.forEach((item, index) => {
+                    if (index === 0) {
+                        item.classList.add('_active')
+                    }
+
+                    createSVGCircle(item)
+
+                    item.style.translate = `-${translate * index}%`
+                    item.style.zIndex = `-${index}`
+
+                    const delay = swiper.slides[index].dataset.delay || DEFAULT_DELAY
+                    item.style.setProperty('--delay', `${parseInt(delay) * 2}s`)
+                })
+
+                // @ts-ignore
+                swiper.params.autoplay.delay = parseInt(swiper.slides[0].dataset.delay || DEFAULT_DELAY) * 1000
+                swiper.autoplay.start()
+            },
+        },
     })
 
-    const circlesPreview = document.querySelectorAll<HTMLElement>('.main-hero__thumbs-slide')
-    const mainSlides = document.querySelectorAll<HTMLElement>('.main-hero__slide')
-
-    const circlesCount = circlesPreview.length
-    const translate = circlesCount <= 5 ? 50 : 70
-
-    /** Позиционирует круглые превью и создает SVG вокруг них */
-    circlesPreview.forEach((item, index) => {
-        if (index === 0) {
-            item.classList.add('_active')
-        }
-
-        item.style.translate = `-${translate * index}%`
-        item.style.zIndex = `-${index}`
-
-        const delay = mainSlides[index].dataset.delay || '3'
-        item.style.setProperty('--delay', `${parseInt(delay) * 2}s`)
-        createSVGCircle(item)
-    })
-
-    function activateCirclePreview(swiper: Swiper) {
-        const activeIndex = swiper.activeIndex
+    function activateCirclePreview() {
+        const activeSlide = document.querySelector('.swiper-slide-active') as SwiperSlideElement
+        const activeIndex = parseInt(activeSlide.dataset.swiperSlideIndex)
 
         const activeCircle = circlesPreview[activeIndex]
         const currentActiveCircle = document.querySelector('.main-hero__thumbs-slide._active') as HTMLElement
 
         currentActiveCircle.classList.remove('_active')
-        currentActiveCircle.style.zIndex = `${-activeIndex}`
-
         activeCircle.classList.add('_active')
         activeCircle.style.zIndex = `${activeIndex}`
+        currentActiveCircle.style.zIndex = `${-activeIndex}`
     }
 
     function setDelayFromSlide(swiper: Swiper) {
-        const currentSlide = swiper.slides[swiper.activeIndex]
-        const delay = currentSlide.dataset.delay || '2'
+        const currentSlide = document.querySelector<HTMLElement>('.swiper-slide-active')
+
+        const delay = currentSlide?.dataset.delay || DEFAULT_DELAY
 
         swiper.autoplay.stop()
         // @ts-ignore
@@ -98,6 +109,9 @@ void (function () {
         swiper.autoplay.start()
     }
 
-    mainSwiper.on('slideChangeTransitionStart', activateCirclePreview)
-    mainSwiper.on('slideChange', setDelayFromSlide)
+    mainSwiper.on('slideChangeTransitionStart', (swiper) => {
+        console.log(swiper.activeIndex)
+        activateCirclePreview()
+        setDelayFromSlide(swiper)
+    })
 })()
